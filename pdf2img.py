f988054307608394154fde1bb6cbfee2e59fa191
mamome2021 <mamome2021@protonmail.com>
2024-04-10 11:36:05 +0800

    Version 11

--- pdf2img.py
+++ pdf2img.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#第10版
+#第11版
 import sys
 import os
 import traceback
@@ -13,6 +13,8 @@ def read_config():
               'single-image': False,
               'no-crop': False,
               'remove-path-fill': False,
+              'extract-jpeg': False,
+              'small-output': False,
               'prefer-mono': False,
               'prefer-png': False,
               'tiff-compression': 'packbits'}
@@ -32,6 +34,10 @@ def read_config():
                 config['no-crop'] = True
             elif option[0] == 'remove-path-fill':
                 config['remove-path-fill'] = True
+            elif option[0] == 'extract-jpeg':
+                config['extract-jpeg'] = True
+            elif option[0] == 'small-output':
+                config['small-output'] = True
             elif option[0] == 'prefer-mono':
                 config['prefer-mono'] = True
             elif option[0] == 'prefer-png':
@@ -43,6 +49,18 @@ def read_config():
     except Exception:
         print(traceback.format_exc())
     return config
+
+def remove_path_fill(doc, page):
+    images = page.get_images(full=True)
+    if len(images) == 0:
+        return
+    image = images[0]
+    image_name = image[7]
+    ref = image[9]
+    stream = doc.xref_stream(ref)
+    stream_split = stream.split(f'/{image_name} Do\n'.encode(), 1)
+    stream_split[0] = stream_split[0].replace(b'\nf\n', b'\nn\n')
+    doc.update_stream(ref, f'/{image_name} Do\n'.encode().join(stream_split))
 
 def find_largest_image(images):
     size = 0
@@ -68,7 +86,7 @@ def generate_image(config, doc, page, page_noimg, image, output_dir):
     cs_type = doc.xref_get_key(img_xref, "ColorSpace")[0]
     cs = doc.xref_get_key(img_xref, "ColorSpace")[1]
     is_mono = False
-    output_name=f"{output_dir}/{page.number+1}-{img_xref}"
+    output_name = f"{output_dir}/{page.number+1}-{img_xref}"
     if doc.xref_get_key(img_xref, "Filter")[1] == '/DCTDecode':
         if cs == "/DeviceCMYK":
             print(output_name, "jpeg-cmyk")
@@ -77,6 +95,8 @@ def generate_image(config, doc, page, page_noimg, image, output_dir):
         else:
             print(output_name, "jpeg")
             pil_image = Image.open(BytesIO(doc.xref_stream_raw(img_xref)))
+            if config['extract-jpeg']:
+                open(f"{output_name}.jpg",'wb').write(doc.xref_stream_raw(img_xref))
     elif doc.xref_get_key(img_xref,"ImageMask")[1] == 'true' or doc.xref_get_key(img_xref, "BitsPerComponent")[1] == '1':
         print(output_name, "mono")
         is_mono = True
@@ -126,8 +146,40 @@ def generate_image(config, doc, page, page_noimg, image, output_dir):
         img_merge.paste(img_noimg, (round(-x_offset * zoom), round(-y_offset * zoom)), img_noimg)
     if is_mono and config['prefer-mono']:
         img_merge = img_merge.point(lambda i: i>127 and 255, mode='1')
-        
-    return img_merge, output_name
+
+    return img_merge
+
+def save_pil_image(config, image, output_name):
+    if config['small-output']:
+        if image.mode == 'CMYK':
+            image.save(f"{output_name}.tiff", compression='tiff_lzw')
+        elif image.mode == '1':
+            io_group4=BytesIO()
+            io_png=BytesIO()
+            io_lzw=BytesIO()
+            image.save(io_group4, format='tiff', compression='group4')
+            image.save(io_png, format='png')
+            image.save(io_lzw, format='tiff', compression='tiff_lzw')
+            if io_lzw.getbuffer().nbytes < io_png.getbuffer().nbytes and io_lzw.getbuffer().nbytes < io_group4.getbuffer().nbytes:
+                image.save(f"{output_name}.tiff", compression='tiff_lzw')
+            elif io_group4.getbuffer().nbytes < io_png.getbuffer().nbytes:
+                image.save(f"{output_name}.tiff", compression='group4')
+            else:
+                image.save(f"{output_name}.png")
+        else:
+            io_lzw=BytesIO()
+            io_png=BytesIO()
+            image.save(io_lzw, format='tiff', compression='tiff_lzw')
+            image.save(io_png, format='png')
+            if io_lzw.getbuffer().nbytes < io_png.getbuffer().nbytes:
+                image.save(f"{output_name}.tiff", compression='tiff_lzw')
+            else:
+                image.save(f"{output_name}.png")
+    else:
+        if config['prefer-png'] == True and image.mode != 'CMYK':
+            image.save(f"{output_name}.png")
+        else:
+            image.save(f"{output_name}.tiff", compression=config['tiff-compression'])
 
 def main():
     if len(sys.argv) == 1:
@@ -139,14 +191,11 @@ def main():
         doc = fitz.open(file)
         doc_noimg = fitz.open(file)
         for page in doc_noimg:
+            if config['remove-path-fill']:
+                remove_path_fill(doc_noimg, page)
             for image in page.get_images():
                 xref = image[0]
                 page.delete_image(xref)
-            if config['remove-path-fill']:
-                ref = page.get_xobjects()[-1][0]
-                stream = doc.xref_stream(ref)
-                stream_new = stream.replace('\nf\n'.encode(), '\nn\n'.encode())
-                doc_noimg.update_stream(ref, stream_new)
         doc_noimg = fitz.open('pdf', doc_noimg.tobytes(garbage=1))
         output_dir = file + "-img"
         os.makedirs(output_dir, exist_ok=True)
@@ -155,24 +204,23 @@ def main():
             try:
                 page_noimg = doc_noimg[pagenum]
                 images = page.get_images()
+                pagenum_str = str(pagenum + 1).zfill(3)
                 if not images:
                     print(f'警告：第{pagenum+1}頁沒有圖片，使用600dpi渲染')
                     image = render_image(config, page, 600 / 72, alpha=False)
-                    if config['prefer-png'] == True:
-                        image.save(f"{output_dir}/{pagenum+1}.png")
-                    else:
-                        image.save(f"{output_dir}/{pagenum+1}.tiff", compression=config['tiff-compression'])
+                    save_pil_image(config, image, f"{output_dir}/{pagenum_str}")
                     continue
                 if len(images) > 1:
                     print(f'警告：第{pagenum+1}頁包含多張圖片，輸出圖片只會包含一張圖片')
                     if config['single-image']:
                         images = [find_largest_image(images)]
                 for image in images:
-                    img_generated, output_name = generate_image(config, doc, page, page_noimg, image, output_dir)
-                    if config['prefer-png'] == True and img_generated.mode != 'CMYK':
-                        img_generated.save(f"{output_name}.png")
+                    img_generated = generate_image(config, doc, page, page_noimg, image, output_dir)
+                    if len(images) == 1:
+                        output_name = f"{output_dir}/{pagenum_str}"
                     else:
-                        img_generated.save(f"{output_name}.tiff", compression=config['tiff-compression'])
+                        output_name = f"{output_dir}/{pagenum_str}-{image[0]}"
+                    save_pil_image(config, img_generated, output_name)
             except Exception as e:
                 print(traceback.format_exc())
                 if config['error']:
